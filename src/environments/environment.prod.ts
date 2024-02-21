817e96364aa03cc520245b963fbcc0300159726b
donmo <donmo@MREDA>
2024-02-21 15:07:05 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'PAS',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44356/',
+    redirectUri: baseUrl,
+    clientId: 'PAS_App',
+    responseType: 'code',
+    scope: 'offline_access PAS',
+    requireHttps: true
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44356',
+      rootNamespace: 'PAS',
+    },
+  },
+} as Environment;
